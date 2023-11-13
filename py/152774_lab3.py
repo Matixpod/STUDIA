f9e23cf659568e94f8c53108f43e739260b09f32
Matixpod <matixpod@gmail.com>
2023-11-13 22:53:44 +0100

    ,.mhd.g

--- py/152774_lab3.py
+++ py/152774_lab3.py
@@ -1,3 +1,14 @@
 print([i for i in range(999)])
 
 print("dupa maka sraka")
+
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
+print('adhasfshfshfjknfksgnkjsngosuohsfs')
